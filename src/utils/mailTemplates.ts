2989e86ea01225ad0bbdf247ba2328b15f80f4cc
HamidTech360 <hammedowolabi2001@gmail.com>
2022-05-15 18:43:26 -0700

    editing

--- src/utils/mailTemplates.ts
+++ src/utils/mailTemplates.ts
@@ -2,7 +2,7 @@ export function CreateHaikuMailTemplate (link:any){
     return (
         `
                 <div style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
-                <h2>You published your Haik</h2>
+                <h2>You published your HAIQ</h2>
                 <p>Here is the link to your haiq ${link}</p>
             </div>
         `
@@ -15,7 +15,7 @@ export function HaikuReceiptMailTemplate (link:String|null=null){
         <div style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;padding-left: 20px;padding-right: 20px;">
             <h1>Memorialization Receipt</h1>
             <p>Hi there</p>
-            <p>Here is your receipt from Modern Haiq</p>
+            <p>Here is your receipt from Modern HAIQ</p>
             <p>Purchase of Authorship and Memorialization. You can view through this link ${link}</p>
             <h2>Total: $100</h2>
         </div>
